a4697b9fd3600268d62ced666f0cffc156bedc85
Mike <83682523+profileaccount@users.noreply.github.com>
2024-04-18 16:58:33 -0400

    Update LinoriaLibrary.lua

--- LinoriaLibrary.lua
+++ LinoriaLibrary.lua
@@ -10,9 +10,9 @@ local Teams = cloneref(game:GetService('Teams'))
 local Players = cloneref(game:GetService('Players'))
 local RunService = cloneref(game:GetService('RunService'))
 local TweenService = cloneref(game:GetService('TweenService'))
-local RenderStepped = cloneref(RunService.RenderStepped)
-local LocalPlayer = cloneref(Players.LocalPlayer)
-local Mouse = cloneref(LocalPlayer:GetMouse())
+local RenderStepped = RunService.RenderStepped
+local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
 
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
