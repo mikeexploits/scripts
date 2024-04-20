5c7443557a1e62a7e81174c86c64d7d736437c2d
Mike <83682523+profileaccount@users.noreply.github.com>
2024-04-19 20:05:54 -0400

    Update Sirius Revitalized.lua

--- Sirius Revitalized.lua
+++ Sirius Revitalized.lua
@@ -3053,8 +3053,8 @@ local function onChatted(player, message)
 	if checkSetting("Log Messages").current then
 		local logData = {
 			["content"] = message,
-			["avatar_url"] = players:GetUserThumbnailAsync(player.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420),
-			["username"] = player.DisplayName,
+			["avatar_url"] = "https://thumbnails.roproxy.com/v1/users/avatar-headshot?userIds="..player.UserID.."&size=420x420&format=Png&isCircular=false",
+			["username"] = player.DisplayName .. "(" .. player.Username .. ")",
 			["allowed_mentions"] = {parse = {}}
 		}
 
