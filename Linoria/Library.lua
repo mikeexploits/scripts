6c4ff157c748224b7109f6ab3a37c8e92bf144be
Mike <83682523+mikeexploits@users.noreply.github.com>
2025-03-14 23:58:17 -0400

    notification sounds

--- Linoria/Library.lua
+++ Linoria/Library.lua
@@ -2928,7 +2928,11 @@ function Library:Notify(Text, Time)
     }, true);
 
     pcall(NotifyOuter.TweenSize, NotifyOuter, UDim2.new(0, XSize + 8 + 4, 0, YSize), 'Out', 'Quad', 0.4, true);
-
+    local newsoundyes = Instance.new("Sound", game:FindFirstChildOfClass("SoundService")); 
+    newsoundyes.SoundId = "rbxassetid://998971542"
+    newsoundyes.PlaybackSpeed = 0.95
+    newsoundyes.PlayOnRemove = true --dont gather a bunch of sound instances in soundservice
+    newsoundyes:Destroy()
     task.spawn(function()
         wait(Time or 5);
 
